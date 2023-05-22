41c993cf653ec010fa46486d32b96b9035d82666
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-05-22 19:29:52 +0530

    remove react-dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 import { AuthContextProvider } from "./context/authContext";
-import dotenv from 'react-dotenv';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
