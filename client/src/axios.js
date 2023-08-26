a4fac85d4571f56fd89371715ad39321226a5008
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-08-26 13:31:35 +0530

    update api url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const makeRequest = axios.create({
-        baseURL: 'http://localhost:5000/api',
+        baseURL: process.env.REACT_APP_API_URL,
         withCredentials: true,
 })
