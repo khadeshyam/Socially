7465e23ce295758dd6bee0b006e4564da270e2d7
SHYAM KHADE <khade.shyam10.1@gmail.com>
2023-12-14 21:53:02 +0530

    Add pagination to getPosts

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -3,18 +3,21 @@ import jwt from 'jsonwebtoken';
 import moment from 'moment';
 
 export const getPosts = (req, res) => {
-
     const userId = req.query?.userId;
+    const page = req.query?.page || 1;
+    const limit = req.query?.limit || 3;
+    const offset = (page - 1) * limit;
+
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json({ message: 'Not logged in' });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, userInfo) => {
         if (err) return res.status(403).json({ message: 'Session Expired' });
 
-        const q = userId ?
-            'SELECT p.*,u.id AS userId ,u.name AS userName ,u.profilePic AS userProfilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.id DESC'
-            : 'SELECT p.*,u.id AS userId ,u.name AS userName ,u.profilePic AS userProfilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) ORDER BY p.id DESC';
-        const values = userId ? [userId] : [];
+        const q = userId
+            ? 'SELECT p.*,u.id AS userId ,u.name AS userName ,u.profilePic AS userProfilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.id DESC LIMIT ? OFFSET ?'
+            : 'SELECT p.*,u.id AS userId ,u.name AS userName ,u.profilePic AS userProfilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) ORDER BY p.id DESC LIMIT ? OFFSET ?';
+        const values = userId ? [userId, limit, offset] : [limit, offset];
         db.query(q, values, (err, data) => {
             if (err) {
                 console.log(err);
@@ -22,10 +25,9 @@ export const getPosts = (req, res) => {
             } else {
                 res.status(200).json(data);
             }
-        })
+        });
     });
-}
-
+};
 export const addPost = (req, res) => {
     const token = req.cookies.accessToken;
     if (!token) return res.status(401).json({ message: 'Not logged in' });
